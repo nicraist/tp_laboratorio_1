db73fc195376da070952bad8bacdcb5f680fba3b
nicraist <30535903+nicraist@users.noreply.github.com>
2017-09-07 19:31:16 -0300

    Update funciones.h

--- TP_1_Cascara/funciones.h
+++ TP_1_Cascara/funciones.h
@@ -2,10 +2,45 @@
 #define FUNCIONES_H_INCLUDED
 
 int hacer_suma(int x, int y);
+/** \brief hace la suma de dos numeros 
+ *
+ * \param primer parametro es x, donde es un entero que recibe
+ * \param segundo parametro es y, donde tambien es un entero que recibe
+ * \return entrega la suma  entre x e y, que es int
+ *
+ */     
 int hacer_resta(int x, int y);
+/** \brief hace la resta de dos numeros 
+ *
+ * \param primer parametro es x, donde es un entero que recibe
+ * \param segundo parametro es y, donde tambien es un entero que recibe
+ * \return entrega la resta  entre x e y, que es int
+ *
+ */     
 float hacer_division(int x, int y);
+/** \brief hace la division de dos numeros 
+ *
+ * \param primer parametro es x, donde es un entero que recibe
+ * \param segundo parametro es y, donde tambien es un entero que recibe
+ * \return entrega la division  entre x e y, que es float
+ *
+ */        
 int hacer_multiplicacion(int x, int y);
+/** \brief multiplica los  dos numeros  
+ *
+ * \param primer parametro es x, donde es un entero que recibe
+ * \param segundo parametro es y, donde tambien es un entero que recibe
+ * \return entrega la multiplicacion entre x e y, que es int
+ *
+ */         
 long long int hacer_factorial(int x);
+/** \brief hace el factorial de un numero 
+ *
+ * \param primer parametro es x, donde es un entero que recibe
+ * \param -
+ * \return entrega el factorial de x, de long long int
+ *
+ */         
 
 
 
