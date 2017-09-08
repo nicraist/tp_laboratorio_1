42dcf87a878bd9a4b56aad11a95309edeb823713
nicraist <30535903+nicraist@users.noreply.github.com>
2017-09-08 11:23:23 -0300

    Update funciones.h

--- TP_1_Cascara/funciones.h
+++ TP_1_Cascara/funciones.h
@@ -21,11 +21,32 @@ float hacer_resta(float x, float y);
  */
 
 float hacer_division(float x, float y);
+/** \brief esta funcion hace la division de "x" con el numero "y"
+ *
+ * \param el primer valor es de tipo float
+ * \param segundo valor es de tipo float
+ * \return devuelve la division total entre "x" e "y"
+ *
+ */
 
 float hacer_multiplicacion(float x, float y);
+/** \brief esta funcion hace la multiplicacion de "x" con el numero "y"
+ *
+ * \param el primer valor es de tipo float
+ * \param segundo valor es de tipo float
+ * \return devuelve la multiplicacion total entre "x" e "y"
+ *
+ */
 
 
 long long int hacer_factorial(float x);
+/** \brief esta funcion hace el factorial de x
+ *
+ * \param el primer valor es de tipo float
+ * \param -
+ * \return devuelve factorial de x
+ *
+ */
 
 
 
